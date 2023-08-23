7d796a4215e5ede601d441267bc29e30da24ec00
adiyat-shihab <mohammadshihab104@gmail.com>
2023-08-24 00:28:45 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const navigation = document.querySelectorAll(".rongin");
+for (const el of navigation) {
+  el.classList.add(
+    "hover:text-[#FE4367]",
+    "cursor-pointer",
+    "transition-all",
+    "duration-300"
+  );
+}
